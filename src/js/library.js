89cec82fa06949b0e990b95d022a5f056d30eb20
Владимир Перетятько <djbob2000@gmail.com>
2022-12-08 08:53:51 +0200

    library2

--- src/js/library.js
+++ src/js/library.js
@@ -1,3 +1,6 @@
+import createGalleryMarkup from './create-gallery';
+import getGenreNames from './create-gallery';
+
 const refs = {
   btnWatched: document.querySelector('.watched'),
   btnQueue: document.querySelector('.queue'),
@@ -41,7 +44,7 @@ function onWatchedButtonClick(event) {
     refs.btnWatched.classList.add('user-lib-btn--active');
   }
   refs.noItemsImg.style.display = 'none';
-  createLibraryGalleryMarkup(watchedMovies);
+  createGalleryMarkup(watchedMovies);
   refs.loader.classList.add('is-hidden');
 }
 
@@ -54,31 +57,31 @@ function onQueueButtonClick(event) {
     refs.btnQueue.classList.add('user-lib-btn--active');
   }
   refs.noItemsImg.style.display = 'none';
-  createLibraryGalleryMarkup(queueMovies);
+  createGalleryMarkup(queueMovies);
   refs.loader.classList.add('is-hidden');
 }
 
-function createLibraryGalleryMarkup(imagesArray) {
-  console.log(imagesArray);
-  refs.libraryGallery.innerHTML = imagesArray
-    .map(image => {
-      const { poster_path, title, genre, year } = image;
-      return `
-               <div class="card">
-                
-                    <img class="card__poster" src="${poster_path}" alt=""  loading="lazy" width="320px" height="210px"/>
-                   
-                    <div  class="card__info">
-                        <p class="info__title"><b>${title}</b><br/>
-                        </p>
-                        <p ><b class="info__genre">${genre}</b>
-                       <span class="info__span"> | </span>
-                        <b class="info__release-date">${year}</b>
-                        </p>
-                        
-                    </div>
-                </div>
-            `;
-    })
-    .join('');
-}
+// function createLibraryGalleryMarkup(imagesArray) {
+//   console.log(imagesArray);
+//   refs.libraryGallery.innerHTML = imagesArray
+//     .map(image => {
+//       const { poster_path, title, genre, year } = image;
+//       return `
+//                <div class="card">
+
+//                     <img class="card__poster" src="${poster_path}" alt=""  loading="lazy" width="320px" height="210px"/>
+
+//                     <div  class="card__info">
+//                         <p class="info__title"><b>${title}</b><br/>
+//                         </p>
+//                         <p ><b class="info__genre">${genre}</b>
+//                        <span class="info__span"> | </span>
+//                         <b class="info__release-date">${year}</b>
+//                         </p>
+
+//                     </div>
+//                 </div>
+//             `;
+//     })
+//     .join('');
+// }
