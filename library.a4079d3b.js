0a99bc076a2a637fa71ec15508e0657495170c7c
DonKanElion <95186395+DonKanElion@users.noreply.github.com>
2023-01-05 17:31:43 +0000

    Deploying to gh-pages from @ DonKanElion/Filmoteka@909a61794034b749fc1afc4e6dfb9515b6f6bc29 🚀

---
+++ library.a4079d3b.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector(".footer__btn-team"),closeModalBtn:document.querySelector(".modal__close-btn"),modalTeam:document.querySelector(".backdrop__team")};function t(){e.modalTeam.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t),window.addEventListener("click",(function(e){e.target===document.querySelector(".backdrop__team")&&t()}))}();
+//# sourceMappingURL=library.a4079d3b.js.map
