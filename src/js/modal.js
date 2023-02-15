a0014a2450a3848bfb34e8e9a9913dd874186f16
Владимир Перетятько <djbob2000@gmail.com>
2023-02-15 19:44:10 +0200

    quotas fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,92 +1,77 @@
 import { getGenreNames } from './create-gallery';
-import {
-  storageKeys,
-  buttonStates,
-  storageHasMovie,
-  addWatchedBtn,
-  addQueueBtn,
-} from './storage';
+import {storageKeys, buttonStates, storageHasMovie, addWatchedBtn, addQueueBtn} from './storage';
 
 /** Зберігається стан кнопок модального вікна */
 export let btnState = {
   watched: false,
   queue: false,
-};
+}
 export let currentMovie;
 
 function openModal() {
-  document.querySelector('.backdrop').style.display = 'block';
+  document.querySelector('.backdrop').style.display = "block";
   document.querySelector('body').style.overflowY = 'hidden';
 
   //Встановлення стилю кнопки "ADD TO WATCHED"
-  if (storageHasMovie(storageKeys.watched, currentMovie.id)) {
-    addWatchedBtn.textContent = buttonStates.on + ' ' + storageKeys.watched;
-    addWatchedBtn.classList.add('active');
+  if(storageHasMovie(storageKeys.watched, currentMovie.id)){
+    addWatchedBtn.textContent = buttonStates.on + " " + storageKeys.watched;
+    addWatchedBtn.classList.add("active");
     btnState.watched = true;
-  } else {
-    addWatchedBtn.textContent = buttonStates.off + ' ' + storageKeys.watched;
-    addWatchedBtn.classList.remove('active');
+  }
+  else{
+    addWatchedBtn.textContent = buttonStates.off + " " + storageKeys.watched;
+    addWatchedBtn.classList.remove("active");
     btnState.watched = false;
   }
 
   //Встановлення стилю кнопки "ADD TO QUEUE"
-  if (storageHasMovie(storageKeys.queue, currentMovie.id)) {
-    addQueueBtn.textContent = buttonStates.on + ' ' + storageKeys.queue;
-    addQueueBtn.classList.add('active');
+  if(storageHasMovie(storageKeys.queue, currentMovie.id)){
+    addQueueBtn.textContent = buttonStates.on + " " + storageKeys.queue;
+    addQueueBtn.classList.add("active");
     btnState.queue = true;
-  } else {
-    addQueueBtn.textContent = buttonStates.off + ' ' + storageKeys.queue;
-    addQueueBtn.classList.remove('active');
+  }
+  else{
+    addQueueBtn.textContent = buttonStates.off + " " + storageKeys.queue;
+    addQueueBtn.classList.remove("active");
     btnState.queue = false;
   }
 }
 
 function closeModal() {
-  document.querySelector('.backdrop').style.display = 'none';
+  document.querySelector('.backdrop').style.display = "none";
   document.querySelector('body').style.overflowY = 'visible';
 }
 
-document.querySelector('.modal__close').addEventListener('click', closeModal);
-document.querySelector('.gallery').addEventListener('click', function (e) {
+document.querySelector('.modal__close').addEventListener("click", closeModal);
+document.querySelector('.gallery').addEventListener("click", function(e) {
   let targetItem = e.target;
-  if (targetItem.closest('.card')) {
+  if (targetItem.closest('.card')){
     const movieId = targetItem.closest('.card').getAttribute('movie-id');
     const data = JSON.parse(localStorage.getItem('movieData'));
 
-    const movie = data.find(item => {
+    const movie = data.find((item)=> {
       if (Number(movieId) === item.id) {
         return true;
       }
-    });
+    })
     currentMovie = movie;
 
-    document
-      .querySelector('.content-card__img>img')
-      .setAttribute(
-        'src',
-        'https://image.tmdb.org/t/p/w500' + movie.poster_path
-      );
+    document.querySelector('.content-card__img>img').setAttribute('src', 'https://image.tmdb.org/t/p/w500' + movie.poster_path);
     document.querySelector('.content-card__title').innerText = movie.title;
-    document.querySelector('.content-card__about-text').innerText =
-      movie.overview;
+    document.querySelector('.content-card__about-text').innerText = movie.overview;
 
-    document.querySelector('.modal__movie-bord').innerText =
-      movie.vote_average.toFixed(1);
+    document.querySelector('.modal__movie-bord').innerText = movie.vote_average.toFixed(1);
     document.querySelector('.modal__movie-number').innerText = movie.vote_count;
-    document.querySelector('.modal__movie-popularity').innerText =
-      movie.popularity.toFixed(1);
-    document.querySelector('.modal__movie-original').innerText =
-      movie.original_title;
-    document.querySelector('.modal__movie-genres').innerText = getGenreNames(
-      movie.genre_ids
-    );
+    document.querySelector('.modal__movie-popularity').innerText = movie.popularity.toFixed(1);
+    document.querySelector('.modal__movie-original').innerText = movie.original_title;
+    document.querySelector('.modal__movie-genres').innerText = getGenreNames(movie.genre_ids);
 
     openModal();
   }
 });
 
 //закриття модалки по кліку поза модалки
-window.addEventListener('click', function (event) {
+window.addEventListener("click", function(event) {
   if (event.target === document.querySelector('.backdrop')) {
     closeModal();
   }
@@ -94,8 +79,9 @@ window.addEventListener('click', function (event) {
 
 //закрытие модалки по esc
 
-window.addEventListener('keydown', e => {
-  if (e.key === 'Escape') {
-    closeModal();
-  }
-});
+ window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+
+     closeModal();
+   }
+ })
