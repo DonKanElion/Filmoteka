f3fc4d336cea54a7d651e6ee6e2dfd31758e1cfe
Владимир Перетятько <djbob2000@gmail.com>
2023-03-01 01:15:42 +0200

    fix undefined page

--- src/js/library.js
+++ src/js/library.js
@@ -1,41 +1,41 @@
-import { createGalleryMarkupLibrary } from "./create-gallery-library";
-import { getLocalStorage } from "./LocaleStorageApi";
+import { createGalleryMarkupLibrary } from './create-gallery-library';
+import { getLocalStorage } from './LocaleStorageApi';
 
-import Pagination from "tui-pagination";
-import { paginationOptions } from "./projectOptions";
-let KEY = "";
-export let moviesFromStorage = "";
+import Pagination from 'tui-pagination';
+import { paginationOptions } from './projectOptions';
+let KEY = '';
+export let moviesFromStorage = '';
 const CARDS_PER_PAGE = 1000;
 const refs = {
-  btnWatched: document.querySelector(".watched"),
-  btnQueue: document.querySelector(".queue"),
-  btnList: document.querySelector(".library-header__buttons"),
-  libraryGallery: document.querySelector(".gallery"),
-  loader: document.querySelector(".loader"),
-  noItemsImg: document.querySelector(".library__no-items"),
-  tuiContainer: document.getElementById("tui-pagination-container"),
+  btnWatched: document.querySelector('.watched'),
+  btnQueue: document.querySelector('.queue'),
+  btnList: document.querySelector('.library-header__buttons'),
+  libraryGallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+  noItemsImg: document.querySelector('.library__no-items'),
+  tuiContainer: document.getElementById('tui-pagination-container'),
 };
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   refs.btnQueue.click();
 });
 
-refs.btnList.addEventListener("click", onBtnClick);
+refs.btnList.addEventListener('click', onBtnClick);
 
 function onBtnClick(evt) {
   try {
     let { name: key, tagName } = evt.target;
     KEY = key.toUpperCase();
-    if (tagName !== "BUTTON") return;
+    if (tagName !== 'BUTTON') return;
 
     switch (KEY) {
-      case "QUEUE":
-        refs.btnQueue.classList.add("library-header__button--active");
-        refs.btnWatched.classList.remove("library-header__button--active");
+      case 'QUEUE':
+        refs.btnQueue.classList.add('library-header__button--active');
+        refs.btnWatched.classList.remove('library-header__button--active');
         break;
-      case "WATCHED":
-        refs.btnWatched.classList.add("library-header__button--active");
-        refs.btnQueue.classList.remove("library-header__button--active");
+      case 'WATCHED':
+        refs.btnWatched.classList.add('library-header__button--active');
+        refs.btnQueue.classList.remove('library-header__button--active');
         break;
       default:
         break;
@@ -45,7 +45,7 @@ function onBtnClick(evt) {
     const totalCards = moviesFromStorage.length;
     const totalPages = Math.ceil(totalCards / CARDS_PER_PAGE);
     if (totalPages < 2) {
-      refs.tuiContainer.style.display = "none";
+      refs.tuiContainer.style.display = 'none';
     }
 
     const paginaton = new Pagination(refs.tuiContainer, {
@@ -53,21 +53,22 @@ function onBtnClick(evt) {
       totalItems: totalPages,
     });
 
-    paginaton.on("afterMove", aferMovePagination);
+    paginaton.on('afterMove', aferMovePagination);
 
     if (!moviesFromStorage.length) {
-      refs.libraryGallery.innerHTML = "";
-      refs.noItemsImg.style.display = "block";
-      refs.loader.classList.add("is-hidden");
+      refs.libraryGallery.innerHTML = '';
+      refs.noItemsImg.style.display = 'block';
+      refs.loader.classList.add('is-hidden');
       return;
     }
 
-    refs.loader.classList.add("is-hidden");
-    refs.noItemsImg.style.display = "none";
+    refs.loader.classList.add('is-hidden');
+    refs.noItemsImg.style.display = 'none';
     const sliced = cutDataForPagination(moviesFromStorage);
-    makePagination((page = 1), sliced);
+    makePagination(1, sliced);
   } catch (error) {
-    refs.libraryGallery.innerHTML = "";
+    console.log(error);
+    refs.libraryGallery.innerHTML = '';
   } finally {
     return KEY, moviesFromStorage;
   }
@@ -75,10 +76,10 @@ function onBtnClick(evt) {
 
 export function refreshLibrary() {
   switch (KEY) {
-    case "QUEUE":
+    case 'QUEUE':
       refs.btnQueue.click();
       break;
-    case "WATCHED":
+    case 'WATCHED':
       refs.btnWatched.click();
       break;
     default:
